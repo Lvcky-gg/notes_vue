86b99ec839f9339710154ff45bf81a27b6ee3045
lvcky-gg <matadorcrab@gmail.com>
2024-04-15 13:44:02 -0400

    commit

--- src/views/ViewNotes.vue
+++ src/views/ViewNotes.vue
@@ -6,7 +6,7 @@
           <textarea class="textarea" placeholder="Textarea"></textarea>
         </div>
       </div>
-      <div class="field is-grouped">
+      <div class="field is-grouped is-grouped-right">
         <div class="control">
           <button class="button is-link">Submit</button>
         </div>
