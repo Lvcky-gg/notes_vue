d681bfcf10199e6ff77250b68a6a078924f9af7f
lvcky-gg <matadorcrab@gmail.com>
2024-04-15 12:46:18 -0400

    commit

--- src/router.js
+++ src/router.js
@@ -1,7 +1,14 @@
 import { createRouter, createWebHashHistory } from "vue-router";
+import ViewNotes from "./views/ViewNotes.vue";
 const router = createRouter({
   history: createWebHashHistory(),
-  routes: [],
+  routes: [
+    {
+      path: "/",
+      name: "Notes",
+      component: ViewNotes,
+    },
+  ],
 });
 
 export default router;
---
+++ src/views/ViewNotes.vue
@@ -0,0 +1,3 @@
+<template>
+  <div class="notes"><h1>Notes</h1></div>
+</template>
