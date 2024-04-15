b2928fe51a4fe21d306b75377c56933a4e7eec26
lvcky-gg <matadorcrab@gmail.com>
2024-04-15 13:10:47 -0400

    commit

--- src/components/Layout/NavBar.vue
+++ src/components/Layout/NavBar.vue
@@ -45,33 +45,14 @@
     </div>
 
     <div id="navbarBasicExample" class="navbar-menu">
-      <div class="navbar-start">
-        <a class="navbar-item"> Home </a>
-
-        <a class="navbar-item"> Documentation </a>
-
-        <div class="navbar-item has-dropdown is-hoverable">
-          <a class="navbar-link"> More </a>
-
-          <div class="navbar-dropdown">
-            <a class="navbar-item"> About </a>
-            <a class="navbar-item is-selected"> Jobs </a>
-            <a class="navbar-item"> Contact </a>
-            <hr class="navbar-divider" />
-            <a class="navbar-item"> Report an issue </a>
-          </div>
-        </div>
-      </div>
-
       <div class="navbar-end">
-        <div class="navbar-item">
-          <div class="buttons">
-            <a class="button is-primary">
-              <strong>Sign up</strong>
-            </a>
-            <a class="button is-light"> Log in </a>
-          </div>
-        </div>
+        <RouterLink to="/" class="navbar-item" active-class="is-active"
+          >Notes</RouterLink
+        >
+        |
+        <RouterLink to="/stats" class="navbar-item" active-class="is-active"
+          >Stats</RouterLink
+        >
       </div>
     </div>
   </nav>
