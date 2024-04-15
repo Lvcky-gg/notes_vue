bb17ecff17bdb017b2ece163c9436e4b689b4cab
lvcky-gg <matadorcrab@gmail.com>
2024-04-15 13:38:05 -0400

    commit

--- src/views/ViewNotes.vue
+++ src/views/ViewNotes.vue
@@ -1,22 +1,24 @@
 <template>
-  <div class="card" v-for="i in 3">
-    <header class="card-header">
-      <p class="card-header-title">Component</p>
-      <button class="card-header-icon" aria-label="more options">
-        <span class="icon">
-          <i class="fas fa-angle-down" aria-hidden="true"></i>
-        </span>
-      </button>
-    </header>
-    <div class="card-content">
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta nam totam
-      ducimus! Dolorem doloremque nesciunt error. Animi fugit maiores numquam
-      ducimus tenetur aut eos repudiandae! Adipisci voluptatibus laboriosam
-      harum totam?
+  <div class="notes">
+    <div v-for="i in 3" class="card mb-4">
+      <header class="card-header">
+        <p class="card-header-title">Component</p>
+        <button class="card-header-icon" aria-label="more options">
+          <span class="icon">
+            <i class="fas fa-angle-down" aria-hidden="true"></i>
+          </span>
+        </button>
+      </header>
+      <div class="card-content">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta nam
+        totam ducimus! Dolorem doloremque nesciunt error. Animi fugit maiores
+        numquam ducimus tenetur aut eos repudiandae! Adipisci voluptatibus
+        laboriosam harum totam?
+      </div>
+      <footer class="card-footer">
+        <a href="#" class="card-footer-item">Edit</a>
+        <a href="#" class="card-footer-item">Delete</a>
+      </footer>
     </div>
-    <footer class="card-footer">
-      <a href="#" class="card-footer-item">Edit</a>
-      <a href="#" class="card-footer-item">Delete</a>
-    </footer>
   </div>
 </template>
