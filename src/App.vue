afa0b80f22cbd8cfa65d756c5aa1ba0465f6eabe
lvcky-gg <matadorcrab@gmail.com>
2024-04-15 13:04:15 -0400

    commit

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,14 @@
 <template>
-  <RouterLink to="/" class="button">Notes</RouterLink> |
-  <RouterLink to="/stats" class="button">Stats</RouterLink>
+  <!-- <RouterLink to="/" class="button">Notes</RouterLink> |
+  <RouterLink to="/stats" class="button">Stats</RouterLink> -->
+  <NavBar />
   <RouterView />
 </template>
+
+<script setup>
+import NavBar from './components/Layout/NavBar.vue'
+</script>
+
 <style>
 @import './node_modules/bulma/css/bulma.min.css';
 </style>
---
+++ src/components/Layout/NavBar.vue
@@ -0,0 +1,4 @@
+<template>
+  <RouterLink to="/" class="button">Notes</RouterLink> |
+  <RouterLink to="/stats" class="button">Stats</RouterLink>
+</template>
