0f1ac7dea0b10cc3c04f601c7bdc51649d94f178
lvcky-gg <matadorcrab@gmail.com>
2024-04-15 15:34:34 -0400

    commit

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 // import "./assets/main.css";
 import router from "./router";
 import { createApp } from "vue";
-
+import { createPinia } from "pinia";
 import App from "./App.vue";
 
-createApp(App).use(router).mount("#app");
+createApp(App).use(router).use(createPinia()).mount("#app");
