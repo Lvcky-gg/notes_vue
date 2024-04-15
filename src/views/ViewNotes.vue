bf2affb2e732de8d222544a143fbf5a0815cc94d
lvcky-gg <matadorcrab@gmail.com>
2024-04-15 13:41:17 -0400

    commit

--- src/views/ViewNotes.vue
+++ src/views/ViewNotes.vue
@@ -1,5 +1,43 @@
 <template>
   <div class="notes">
+    <div class="field">
+      <label class="label">Message</label>
+      <div class="control">
+        <textarea class="textarea" placeholder="Textarea"></textarea>
+      </div>
+    </div>
+
+    <div class="field">
+      <div class="control">
+        <label class="checkbox">
+          <input type="checkbox" />
+          I agree to the <a href="#">terms and conditions</a>
+        </label>
+      </div>
+    </div>
+
+    <div class="field">
+      <div class="control">
+        <label class="radio">
+          <input type="radio" name="question" />
+          Yes
+        </label>
+        <label class="radio">
+          <input type="radio" name="question" />
+          No
+        </label>
+      </div>
+    </div>
+
+    <div class="field is-grouped">
+      <div class="control">
+        <button class="button is-link">Submit</button>
+      </div>
+      <div class="control">
+        <button class="button is-link is-light">Cancel</button>
+      </div>
+    </div>
+
     <div v-for="i in 3" class="card mb-4">
       <header class="card-header">
         <p class="card-header-title">Component</p>
